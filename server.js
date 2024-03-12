b399c84af0d0cc469b2075d063c4892c30d99553
Unnati101 <143032635+Unnati101@users.noreply.github.com>
2024-03-12 23:13:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ import authRoutes from './routes/authRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
 //configuring env
 dotenv.config();
 
+
+
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
